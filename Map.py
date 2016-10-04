c67305a57e097264c126f332583620a0478a2e0d
mparab01 <infoeastindia1@gmail.com>
2016-10-04 18:05:14 -0500

    Add files via upload

---
+++ Map.py
@@ -0,0 +1,52 @@
+
+# coding: utf-8
+
+# MAP : Built-In Functions in Python
+
+# In[4]:
+
+l1 = range(10)
+l2 = range(5)
+print l1
+print l2
+
+
+# In[5]:
+
+map(lambda x : (9/5)*x +32, l)
+
+
+# In[6]:
+
+l3 = (1,2,3,4,5)
+
+
+# In[7]:
+
+print l3
+
+
+# In[9]:
+
+map(lambda x : x+20, l3)
+
+
+# In[10]:
+
+d1 = {1:'one', 2:'two'}
+
+
+# In[11]:
+
+print d1
+
+
+# In[12]:
+
+map(lambda x : x+5, d1)
+
+
+# In[ ]:
+
+
+
